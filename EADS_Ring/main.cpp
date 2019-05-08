1eaf90b62cf957d3b9e1cb8f997cc0d8fdab8855
Barny <BarnyK@users.noreply.github.com>
2019-05-08 12:24:29 +0200

    Initial Commit

---
+++ EADS_Ring/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EADS_Ring
+//
+//  Created by Winky Face on 08/05/2019.
+//  Copyright © 2019 Barnaba Krupowicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
